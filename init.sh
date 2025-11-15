0e7deee01528bf35f5a73154fba99108a8e7842a
Krzysztof Staroń <kisiel3141@gmail.com>
2025-11-15 13:25:09 +0100

    ok

--- init.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/KrzysztofStaron/PromptCoT-implementation.git
-cd PromptCoT-implementation
-pip install -r requirements.txt
-python test.py
-
-
-python cold_start.py
-python em.py
-
