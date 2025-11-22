3d552c14a0c5144bfc00aaa2be5284039897cb36
Krzysztof Staroń <kisiel3141@gmail.com>
2025-11-22 12:07:43 +0100

    FUCK

--- train_phase1_q.py
+++ train_phase1_q.py
@@ -80,7 +80,7 @@ def train_model(mode, output_dir, hf_path, dataset):
         random_state=3407,
         use_rslora=False,
         loftq_config=None,
-        modules_to_save=["embed_tokens", "lm_head"],  # FULLY train embeddings & output (not quantized!)
+        modules_to_save=None
     )
     
     # Load Phase 0 Adapters from HuggingFace
