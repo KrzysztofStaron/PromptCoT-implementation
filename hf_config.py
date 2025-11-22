96c9d7468472b34e1cf02592033a2570ce9233d7
Krzysztof Staroń <kisiel3141@gmail.com>
2025-11-22 01:20:59 +0100

    aaa

--- hf_config.py
+++ hf_config.py
@@ -3,7 +3,7 @@
 
 # HuggingFace Hub configuration
 HF_USERNAME = "PanzerBread/PromptCoT"
-HF_VERSION = "coding-0.1"  # Set this constant to version your models
+HF_VERSION = "coding-0.2"  # Set this constant to version your models
 HF_TAGS = ["math", "coding"]  # Tags to add to uploaded models
 
 # Computed HuggingFace repo paths (base paths)
